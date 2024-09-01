769d158e357aafdcfa8064d8ae788ca2da3901d8
Kyle Zhang <elykz@Kyles-MacBook-Pro.local>
2024-09-01 14:34:08 -0400

    email added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,15 +3,21 @@ import Link from "next/link"
 const Contact = () => {
   return (
     <div className=" flex flex-col">
-      <div className="text-center py-32">
+      <div className="text-center py-24">
               Have a Question?
       </div>
       <div className=" text-9xl text-center justify-between hover:text-amber-800">
               <Link href='mailto:HLinc.org@gmail.com'>
-                CONTACT US!
+                CONTACT US
               </Link>
+      </div>
+      <div className="flex flex-col">
+          <div className="text-center pt-52 pb-10">
+              Not Sending? Try emailing 
+              <p className="font-bold text-2xl"> Hlinc.org@gmail.com</p>
           </div>
       </div>
+    </div>
   )
 }
 
